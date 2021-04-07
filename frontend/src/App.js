dc4988aa18436dc84b222009b5c8cc4c983210dd
denisjovic <denis.jovic@tn-tech.co.rs>
2021-04-07 13:39:52 +0200

    Add image slider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import Slider from './components/pet-profile/Slider';
+import { SliderData } from './components/pet-profile/SliderData';
 
 function App() {
   return (
     <div className='App'>
-      <h1>udomi ljubimca</h1>
-      <h2>test</h2>
+      <h1>Udomi ljubimca</h1>
+      <Slider slides={SliderData} />
     </div>
   );
 }
