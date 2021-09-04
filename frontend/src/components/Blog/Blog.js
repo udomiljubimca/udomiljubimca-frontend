88f0f7237fb02d37690fe271be4ffd27e877ce5e
Jovan Jonic <jovanjonic@gmail.com>
2021-09-04 15:28:55 +0200

    blog overview page design, pagination, hero component

--- frontend/src/components/Blog/Blog.js
+++ frontend/src/components/Blog/Blog.js
@@ -1,30 +1,56 @@
-import { React } from "react";
+import { React, useState } from "react";
 import BlogCard from "./BlogCard";
-import blogHeader from "./assets/blog-hero.svg";
+import Hero from "../Sections/Hero";
+import BlogBg from "../../assets/blog.png";
 import "./blog.css";
+import ReactPaginate from "react-paginate";
 
 const Blog = ({ blogPosts }) => {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const POST_PER_PAGE = 2;
+  const offset = currentPage * POST_PER_PAGE;
+  const pageCount = Math.ceil(blogPosts.length / POST_PER_PAGE);
+
+  const handlePageClick = ({ selected: selectedPage }) => {
+    setCurrentPage(selectedPage);
+  };
+
   return (
-    <div className="container">
-      <div
-        className="blog-hero flex flex--space-between flex--align-center"
-        data-collapse="desktop"
-      >
-        <div className="blog-hero__info">
-          <h3 className="blog-box-title">BLOG UDOMI LJUBIMCA</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Nam
-            condimentum tempus diam.
-          </p>
-        </div>
-        <img src={blogHeader} alt="Udomi Ljubimca" />
-      </div>
-      <div className="flex flex--space-between flex-wrap">
-        {blogPosts.map((item) => (
-          <BlogCard title={item.title} desc={item.desc} url={item.url} />
+    <>
+      <Hero
+        title={"BLOG"}
+        desc={
+          "Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Nam condimentum tempus diam."
+        }
+        bg={BlogBg}
+      />
+      <div className="container">
+        {blogPosts.slice(offset, offset + POST_PER_PAGE).map((item) => (
+          <BlogCard
+            title={item.title}
+            key={item.title}
+            desc={item.desc}
+            url={item.url}
+          />
         ))}
+        <ReactPaginate
+          previousLabel={"<"}
+          nextLabel={">"}
+          pageCount={pageCount}
+          onPageChange={handlePageClick}
+          containerClassName={
+            "pagination flex flex--justify-center flex--align-center"
+          }
+          previousLinkClassName={"pagination__link"}
+          nextLinkClassName={"pagination__link"}
+          disabledClassName={"pagination__link--disabled"}
+          activeClassName={"pagination__link--active"}
+          pageClassName={"pagination_item"}
+          className="flex"
+        />
       </div>
-    </div>
+    </>
   );
 };
 
--- frontend/src/components/Blog/BlogCard.js
+++ frontend/src/components/Blog/BlogCard.js
@@ -1,18 +1,31 @@
 import { React } from "react";
 import { Link } from "react-router-dom";
 import placeholder from "../../assets/placeholder.svg";
+import "./blogCard.css";
 
 const BlogCard = ({ title, desc, url }) => {
   return (
-    <Link to={`/blog/${url}`} className="blog-card">
-      <img src={placeholder} alt={title} />
-      <h4 className="blog-card-title" title={title}>
-        {title.length > 50 ? title.substr(0, 50 - 1) + "..." : title}
-      </h4>
-      <p className="blog-desc">
-        {desc.length > 100 ? desc.substr(0, 100 - 1) + "..." : desc}
-      </p>
-    </Link>
+    <div className="flex blog-card" data-collapse="tablet">
+      <div className="blog-card-image">
+        <img src={placeholder} alt={title} />
+      </div>
+      <div className="blog-card-info flex flex--justify-center flex--align-center flex-column">
+        <h4 className="blog-card-title title-underline" title={title}>
+          {title.length > 50 ? title.substr(0, 50 - 1) + "..." : title}
+        </h4>
+        <p className="blog-card-desc">
+          {desc.length > 100 ? desc.substr(0, 100 - 1) + "..." : desc}
+        </p>
+        <div className="blog-card-meta flex">
+          <span>10. januar 2021.</span>
+          <span className="blog-card-divider"> | </span>
+          <span>Ime i prezime </span>
+        </div>
+        <Link to={`/blog/${url}`} className="blog-card-link">
+          Pročitaj više
+        </Link>
+      </div>
+    </div>
   );
 };
 
