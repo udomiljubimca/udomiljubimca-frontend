8903e8637fbd148b3e4020a4462e57ded3a20ce7
Maja <majacvetkovic@outlook.com>
2021-11-28 15:21:49 +0100

    profile edit form fields and styles

--- frontend/src/components/UserProfile/UserProfile.js
+++ frontend/src/components/UserProfile/UserProfile.js
@@ -1,29 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UserProfile.css";
 import Avatar from "../../assets/avatar.svg"
+import { AiOutlineEdit, AiOutlineCheck, AiOutlineClose } from 'react-icons/ai'
+import EasyEdit from 'react-easy-edit';
 
 function UserProfile({ userData, isUserView }) {
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const saveEdit = () => {
+    console.log('click');
+  }
+
+  const cancelEdit = () => {
+    console.log('canceled');
+  }
+
+  const handleFieldEdit = (e) => {
+    console.log(e);
+  }
+
+
+
   return (
     <section className="user-profile container">
       <h1 className="page-title mt-4">PROFIL KORISNIKA</h1>
 
       <div className="user-profile__inner">
         <div className="user-profile__intro">
-          <img src={userData.image ? userData.image : Avatar} alt={userData.name} />
+          <div>
+            <img src={userData.image ? userData.image : Avatar} alt={userData.name} />
+            {/* {isEditing && <AiOutlineEdit />} */}
+          </div>
           <h2>{userData.name}</h2>
         </div>
 
         <div className="user-profile__details">
-          <p>{userData.age}, {userData.location}</p>
+          {isEditing ?
+            <EasyEdit
+              type="text"
+              onSave={(e) => saveEdit(e, 'age')}
+              onCancel={cancelEdit}
+              placeholder={userData.age}
+              saveButtonLabel={<AiOutlineCheck />}
+              cancelButtonLabel={<AiOutlineClose />}
+              attributes={{ name: "age", id: 'age' }}
+            /> :
+            <p>{userData.age}</p>
+          }
+
+          {isEditing ?
+            <EasyEdit
+              type="text"
+              onSave={(e) => saveEdit(e, 'location')}
+              onCancel={cancelEdit}
+              placeholder={userData.location}
+              saveButtonLabel={<AiOutlineCheck />}
+              cancelButtonLabel={<AiOutlineClose />}
+              attributes={{ name: "location", id: 'location' }}
+            /> :
+            <p>{userData.location}</p>
+          }
           <p>Korisnik od {userData.registrationDate}.</p>
         </div>
 
         <div className="user-profile__about">
           <h3>O meni</h3>
-          <p>{userData.about}</p>
+
+          {isEditing ?
+            <EasyEdit
+              type="textarea"
+              onSave={(e) => saveEdit(e, 'about')}
+              onCancel={cancelEdit}
+              placeholder={userData.about}
+              saveButtonLabel={<AiOutlineCheck />}
+              cancelButtonLabel={<AiOutlineClose />}
+              attributes={{ name: "about", id: 'about' }}
+            /> :
+            <p>{userData.about}</p>
+          }
+
           {isUserView && (
-            <button type="button" className="custom-btn btn-primary">
-              Uredi profil
+            <button type="button" onClick={() => setIsEditing(prev => !prev)} className="custom-btn btn-primary">
+              {isEditing ? 'Zavrsi' : ' Uredi profil'}
             </button>
           )}
         </div>
--- frontend/src/fakeUserData.js
+++ frontend/src/fakeUserData.js
@@ -2,7 +2,7 @@ export const UserData =
 {
     id: 1,
     name: "Petar Petrovic",
-    age: 33,
+    age: '33',
     location: "Beograd",
     about: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolor totam necessitatibus autem neque accusantium, recusandae cumque suscipit.",
     image: 'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2676&q=80',
